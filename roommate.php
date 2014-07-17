7c0ec7fcf95330b00e91d040f77397e395f17871
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-07-17 23:40:45 +0530

    phps

---
+++ roommate.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Roommate Finder</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <?php
+    $file = ('roomie.txt');
+    $fh = fopen($file, "r") or die('Could not open file!');
+    $data = fread($fh, filesize($file)) or die('Could not read file!');
+    fclose($fh);
+    echo $data;
+    $fh = fopen($file, "w") or die('Could not open file!');
+    fwrite($fh,$data) or die('Could not write to file!');\
+  ?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
